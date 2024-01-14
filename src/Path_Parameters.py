4ad60d2458127d1e7727662c48d4d63a1bf62d93
N4oto-T <h13.only57@gmail.com>
2024-01-14 20:54:59 +0900

    Path Parameters

---
+++ src/Path_Parameters.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+
+from enum import Enum
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+# これを'users/{users_id}'より先に定義する必要がある。でないと、"/users/me"は{"user_id":me}を返すようになる
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Depp Learning FTW"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    return {"model_name": model_name, "message": "Have some resiuals"}
