21852a26d8fd95b8b6f303d2f5681f8ef3d07fc7
N4oto-T <h13.only57@gmail.com>
2024-01-15 21:47:11 +0900

    Path Parameters and Numeric Validations

---
+++ Path_Parameters_and_Numeric_Validations.py
@@ -0,0 +1,23 @@
+from typing import Annotated
+from fastapi import FastAPI, Path, Query
+
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+async def read_items(q: str, item_id: int = Path(title="The ID of the item to get")):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.get("/items2/{item_id}")
+async def read_items(
+    item_id: Annotated[int, Path(title="The ID of the item to get", ge=1)], q: str
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
