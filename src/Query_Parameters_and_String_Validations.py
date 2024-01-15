3d475ce65b0ee994c31d23e8fb6ce9219c031bad
N4oto-T <h13.only57@gmail.com>
2024-01-15 20:16:11 +0900

    --Alias parameters

--- src/Query_Parameters_and_String_Validations.py
+++ src/Query_Parameters_and_String_Validations.py
@@ -1,13 +1,12 @@
-from fastapi import FastAPI, Query
 from typing import Annotated
+
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(
-    q: Annotated[str | None, Query(max_length=50, min_length=3)] = None
-):
+async def read_items(q: Annotated[str | None, Query(alias="item-query")] = None):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
